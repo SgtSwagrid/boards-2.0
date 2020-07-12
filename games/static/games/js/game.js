8e27d3aec187588882a75fa9f7d53c025e3cbf29
Alec Dorrington <alec.dorrington@protonmail.com>
2020-07-12 22:54:02 +0800

    Sidebar now auto-reloads.

--- games/static/games/js/game.js
+++ games/static/games/js/game.js
@@ -1,10 +1,25 @@
-function reload(cx=-1, cy=-1) {
+function loadBoard(cx=-1, cy=-1) {
     $('#board').load('board', 'cx=' + cx + '&cy=' + cy + '&sx=' + sx + '&sy=' + sy);
+}
+
+function loadSidebar(action='') {
+    $('#sidebar').load('sidebar', action);
+}
+
+function sendMessage() {
+    $(document).on('submit', '#message-form', () => {
+        $.get('sidebar?message=' + $('#message')[0].value);
+        $('#message')[0].value = '';
+    });
 }
 
 $(() => {
     $('#board').load('board');
-    setInterval(() => {if(!turn) reload()}, 500);
+    $('#sidebar').load('sidebar');
+    setInterval(() => {
+        if(!turn) loadBoard();
+        loadSidebar();
+    }, 500);
 
     $('#copy_code').click(() => {
         navigator.clipboard.writeText(CODE).then(() => {
@@ -15,4 +30,3 @@ $(() => {
     messages.scrollTop = messages.scrollHeight - messages.clientHeight;
     $('#message').select();
 });
-
