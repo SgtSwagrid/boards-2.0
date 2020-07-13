51fdd5aebdeb89ae236502f85212b15bd98b8d2d
Alec Dorrington <alec.dorrington@protonmail.com>
2020-07-13 23:26:17 +0800

    Update settings.py

--- boards/settings.py
+++ boards/settings.py
@@ -129,7 +129,6 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/users/login'
