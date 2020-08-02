31650a048d6c9e41fb3fb8def849e2f96d976826
Adrian Shedley <adrian.shedley@gmail.com>
2020-08-03 00:14:32 +0800

    Completed Game of the Amazons

--- games/games/amazons.py
+++ games/games/amazons.py
@@ -5,8 +5,8 @@ from .common.handler import *
 class Amazons(Game):
     name = "Amazons"
     id = 6
-    width = 10
-    height = 10
+    width = 4
+    height = 4
     players = 2
 
     class AmazonPiece(PieceType):
@@ -21,13 +21,13 @@ class Amazons(Game):
 
         def move_valid(self, state, piece, x_to, y_to):
             return state.turn.stage == 0 and \
-                not state.pieces[piece.x][piece.y] and \
+                not state.pieces[x_to][y_to] and \
                 is_queen_move(state, piece, x_to, y_to)
 
         def move_piece(self, state, piece, x_to, y_to):
             return state \
                 .move_piece(piece, x_to, y_to) \
-                .state.end_stage()
+                .end_stage()
 
     class ArrowPiece(PieceType):
         id = 1
@@ -49,17 +49,26 @@ class Amazons(Game):
                 is_queen_move(state, queen, piece.x, piece.y)
 
         def place_piece(self, state, piece):
-            return state \
-                .place_piece(self, state.turn.current, piece.x, piece.y) \
-                .end_turn()
+            piece = Piece(self, state.turn.current, piece.x, piece.y)
+            state_next_turn = state.place_piece(piece).end_turn()
+
+            return state_next_turn if state.game.can_move(state_next_turn) \
+                else state_next_turn.end_game(winner=state.turn.current)
 
     types = [AmazonPiece(), ArrowPiece()]
     handlers = [MoveHandler(), PlaceHandler(ArrowPiece())]
 
     def piece(self, x, y):
         # Top and bottom arrangement
-        white_queens = [[3, 0], [6, 0], [0, 3], [9, 3]]
-        black_queens = [[3, 9], [6, 9], [0, 6], [9, 6]]
+
+        tw = self.width // 3
+        th = self.height // 3
+
+        op_tw = self.width - 1 - tw
+        op_th = self.height - 1 - th
+
+        white_queens = [[tw, 0], [op_tw, 0], [0, th], [self.width-1, th]]
+        black_queens = [[tw, self.height-1], [op_tw, self.height-1], [0, op_th], [self.width-1, op_th]]
 
         if [x, y] in white_queens:
             return Piece(self.AmazonPiece(), 0, x, y)  # White
@@ -69,14 +78,19 @@ class Amazons(Game):
 
         return None
 
+    def can_move(self, state):
+        return any([state.open(piece.x + dx, piece.y + dy)
+                    for dx in range(-1, 2)
+                    for dy in range(-1, 2)
+                    for piece in state.pieces_by_player[state.turn.current]
+                    if piece.type.id == self.AmazonPiece().id])
 
-def is_queen_move(state, piece, x_to, y_to):
-    dx, dy = delta(piece.x, piece.y, x_to, y_to)
-    sx, sy = direction(piece.x, piece.y, x_to, y_to)
-    d = distance(piece.x, piece.y, x_to, y_to)
-    return (((sx == 0) ^ (sy == 0)) or (abs(dx) == abs(dy))) and \
-           path(piece.x, piece.y, sx, sy, d, state.pieces)
-
+    def is_queen_move(self, state, piece, x_to, y_to):
+        dx, dy = delta(piece.x, piece.y, x_to, y_to)
+        sx, sy = direction(piece.x, piece.y, x_to, y_to)
+        d = distance(piece.x, piece.y, x_to, y_to)
+        return (((sx == 0) ^ (sy == 0)) or (abs(dx) == abs(dy))) and \
+               path(piece.x, piece.y, sx, sy, d, state.pieces)
 
 def distance(x_from, y_from, x_to, y_to):
     return max(abs(x_to - x_from), abs(y_to - y_from))
@@ -89,7 +103,6 @@ def delta(x_from, y_from, x_to, y_to):
 def direction(x_from, y_from, x_to, y_to):
     return (-1 if x_to < x_from else 0 if x_to == x_from else 1), \
             (-1 if y_to < y_from else 0 if y_to == y_from else 1)
-
 
 def path(x, y, sx, sy, d, pieces):
     return all(map(lambda r: not pieces
--- games/games/common/games.py
+++ games/games/common/games.py
@@ -3,11 +3,13 @@ from games.games.chess import Chess
 from games.games.reversi import Reversi
 from games.games.mill import Mill
 from games.games.connectfour import ConnectFour
+from games.games.amazons import Amazons
 
 games = {
     TicTacToe().id: TicTacToe(),
     Chess().id: Chess(),
     Reversi().id: Reversi(),
     Mill().id: Mill(),
-    ConnectFour().id: ConnectFour()
+    ConnectFour().id: ConnectFour(),
+    Amazons().id: Amazons()
 }
