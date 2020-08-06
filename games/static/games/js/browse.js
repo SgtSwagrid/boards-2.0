f27b1165408cf0d5912165376ef9694b7cf78a04
Alec Dorrington <alec.dorrington@protonmail.com>
2020-08-06 18:51:45 +0800

    Enter key now works for board code field.

--- games/static/games/js/browse.js
+++ games/static/games/js/browse.js
@@ -2,4 +2,10 @@ $(() => {
     $('#goto_board').click(() => {
         location.href = '/games/' + $('#board_code').val();
     });
+
+    $('#board_code').on('keyup', event => {
+        if(event.keyCode == 13) {
+            location.href = '/games/' + $('#board_code').val();
+        }
+    });
 });
